9ab35ecd553e8143bcd60d8dd51bf102598637f6
Matthew Boynes <git@mattboynes.com>
2019-05-08 22:22:57 -0400

    Initial commit of core files

---
+++ byline-manager-sst.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Byline Manager SST Add-On
+ * Plugin URI:      https://github.com/alleyinteractive/byline-manager-sst
+ * Description:     Allow SST to set Bylines
+ * Author:          Matthew Boynes
+ * Author URI:      https://www.alley.co
+ * Text Domain:     byline-manager-sst
+ * Version:         0.1.0
+ *
+ * @package         Byline_Manager_SST
+ */
+
+// Your code starts here.
